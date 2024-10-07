aac7c981a070e8649099f0121633c53a857e276f
Phelipe Cezario <phelipesamuel123@gmail.com>
2024-10-07 00:00:40 -0300

    feat(server): add CRUD operations and tests

--- server/index.js
+++ server/index.js
@@ -8,8 +8,36 @@ const db = mysql2.createConnection({
     user:"root",
     password:"Julio2801.",
     database:"test"
-})
+});
+
+app.use(express.json());
+
+app.get ("/", (req,res)=>{
+    res.json("Hello")
+});
+
+app.get("/books", (req,res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+            return res.json(data)
+    });
+});
+
+app.post("/books", (req,res)=>{
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+    ];
+
+    db.query(q, [values], (err , data) => {
+        if (err) return res.json(err);
+        return res.json("Book has been created.");
+    });
+});
 
 app.listen(8800, ()=>{
     console.log("Connected to backend")
-})
+});
